c1bf04405801957bd2b33218e14628d956aa1368
Sridhar Ratnakumar <srid@srid.ca>
2020-01-26 18:35:11 -0500

    Initial commit

---
+++ default.nix
@@ -0,0 +1,18 @@
+let
+  # To upgrade rib, go to https://github.com/srid/rib/commits/master, select the
+  # revision you would like to upgrade to and set it here. Consult rib's
+  # ChangeLog.md to check any notes on API migration.
+  ribRevision = "a57e7ab7ef5b5b5fcbbf7b99b3edb33bfde5851f";
+
+  inherit (import (builtins.fetchTarball "https://github.com/hercules-ci/gitignore/archive/7415c4f.tar.gz") { }) gitignoreSource;
+in {
+# Rib library source to use
+  rib ? builtins.fetchTarball "https://github.com/srid/rib/archive/${ribRevision}.tar.gz"
+# Cabal project root
+, root ? gitignoreSource ./.
+# Cabal project name
+, name ? "rib-sample"
+, ...
+}:
+
+import rib { inherit root name; }
