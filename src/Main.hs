9ceaa0f1200d011de0f63284a84ed20199b75e79
Sridhar Ratnakumar <srid@srid.ca>
2020-01-26 20:09:24 -0500

    Fetch and generate topic pages

--- src/Main.hs
+++ src/Main.hs
@@ -19,7 +19,8 @@ import Zulip.Client
 
 data Page
   = Page_Index [Stream]
-  | Page_Stream Stream
+  | Page_Stream Stream [Topic]
+  | Page_StreamTopic Stream Topic
 
 main :: IO ()
 main = Rib.runWith [reldir|a|] [reldir|b|] generateSite (Rib.Serve 8080 False)
@@ -30,16 +31,25 @@ generateSite = do
   -- Copy over the static files
   Rib.buildStaticFiles [[relfile|static/**|]]
   [apiKey] <- fmap toText <$> liftIO getArgs
-  streams <- demo apiKey
-  forM_ streams $ \stream -> do
+  d <- demo apiKey
+  forM_ d $ \(stream, topics) -> do
     f <- liftIO $ parseRelFile $ streamHtmlPath stream
-    Rib.writeHtml f $ renderPage $ Page_Stream stream
+    Rib.writeHtml f $ renderPage $ Page_Stream stream topics
+    forM_ topics $ \topic -> do 
+      g <- liftIO $ parseRelFile $ toString $ topicHtmlPath stream topic
+      Rib.writeHtml g $ renderPage $ Page_StreamTopic stream topic 
   -- Write an index.html linking to the aforementioned files.
   Rib.writeHtml [relfile|index.html|] $
-    renderPage (Page_Index streams)
+    renderPage (Page_Index $ fst <$> d)
 
 streamHtmlPath :: (Semigroup s, IsString s) => Stream -> s
 streamHtmlPath stream = streamUrl stream <> "index.html"
+
+topicHtmlPath :: Stream -> Topic -> Text
+topicHtmlPath = topicUrl
+
+topicUrl :: Stream -> Topic -> Text
+topicUrl stream topic = streamUrl stream <> _topicName topic <> ".html"
 
 streamUrl :: (Semigroup s, IsString s) => Stream -> s
 streamUrl stream = show (_streamStreamId stream) <> "/"
@@ -51,7 +61,8 @@ renderPage page = with html_ [lang_ "en"] $ do
     meta_ [httpEquiv_ "Content-Type", content_ "text/html; charset=utf-8"]
     title_ $ case page of
       Page_Index _ -> "Fun Prog Zulip Archive"
-      Page_Stream s -> toHtml $ _streamName s
+      Page_Stream s _ -> toHtml $ _streamName s
+      Page_StreamTopic _s t -> toHtml $ _topicName t
     style_ [type_ "text/css"] $ C.render pageStyle
     stylesheet "https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css"
     stylesheet "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css"
@@ -67,9 +78,20 @@ renderPage page = with html_ [lang_ "en"] $ do
                   toHtml $ _streamName stream
                 with div_ [class_ "description"] $ 
                   toHtml $ _streamDescription stream
-        Page_Stream stream -> do
+        Page_Stream stream topics -> do
           with h1_ [class_ "ui header"] $ toHtml $ _streamName stream
           p_ $ toHtml $ _streamDescription stream
+          with div_ [class_ "ui relaxed list"] $ 
+            forM_ topics $ \topic -> with div_ [class_ "item"] $ do
+              with div_ [class_ "content"] $ do
+                with a_ [class_ "header", href_ ("/" <> topicUrl stream topic)] $
+                  toHtml $ _topicName topic
+
+        Page_StreamTopic stream topic -> do 
+          with h1_ [class_ "ui header"] $ do 
+            toHtml $ _streamName stream 
+            " > "
+            toHtml $ _topicName topic
   where
     _renderMarkdown =
       MMark.render . either error id . MMark.parsePure "<none>"
--- src/Zulip/Client.hs
+++ src/Zulip/Client.hs
@@ -1,11 +1,12 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE DataKinds #-}
 
 module Zulip.Client where
 
+import Prelude hiding (Option)
 import Data.Aeson
 import Data.Aeson.TH
 import Network.HTTP.Req
-import qualified Shower
 import Zulip.Internal
 
 baseUrl :: Text
@@ -27,22 +28,63 @@ data Stream
       }
   deriving (Eq, Show)
 
-$(deriveJSON fieldLabelMod ''Stream)
+data Topics
+  = Topics 
+    { _topicsResult :: Text 
+    , _topicsMsg :: Text 
+    , _topicsTopics :: [Topic] 
+    }
+  deriving (Eq, Show)
 
+data Topic 
+  = Topic 
+    { _topicName :: Text 
+    , _topicMaxId :: Int 
+    }
+  deriving (Eq, Show)
+
+$(deriveJSON fieldLabelMod ''Stream)
 $(deriveJSON fieldLabelMod ''Streams)
 
-demo :: MonadIO m => Text -> m [Stream]
+$(deriveJSON fieldLabelMod ''Topics)
+$(deriveJSON fieldLabelMod ''Topic)
+
+type APIConfig scheme = (Url scheme, Option scheme)
+
+demo :: MonadIO m => Text -> m [(Stream, [Topic])]
 demo apiKey = do
   let auth = basicAuth "srid@srid.ca" $ encodeUtf8 apiKey
-  r <- runReq defaultHttpConfig $ do
-    req
-      GET
-      (https baseUrl /: "api" /: "v1" /: "streams")
-      NoReqBody
-      jsonResponse
-      auth
-  case fromJSON (responseBody r :: Value) of
-    Error s -> error $ toText s
-    Success (v :: Streams) -> do 
-      liftIO $ Shower.printer v
-      pure $ _streamsStreams v
+      apiConfig = (https baseUrl /: "api" /: "v1", auth)
+  liftIO $ putStrLn "Running API request"
+  runReq defaultHttpConfig $ do
+    r <- getStreams apiConfig
+    case r of
+      Error s -> error $ toText s
+      Success (v :: Streams) -> do 
+        let streams = _streamsStreams v
+        forM streams $ \stream -> do 
+          getTopics apiConfig (_streamStreamId stream) >>= \case 
+            Error s -> error $ toText s 
+            Success topics -> do 
+              pure (stream, topics)
+
+getStreams :: MonadHttp m => APIConfig scheme -> m (Result Streams)
+getStreams (apiUrl, auth) = do
+  r <- req
+    GET
+    (apiUrl /: "streams")
+    NoReqBody
+    jsonResponse
+    auth
+  pure $ fromJSON (responseBody r :: Value)
+
+getTopics :: MonadHttp m => APIConfig scheme -> Int -> m (Result [Topic])
+getTopics (apiUrl, auth) streamId = do
+  r <- req 
+    GET
+    (apiUrl /: "users" /: "me" /: show streamId /: "topics")
+    NoReqBody 
+    jsonResponse 
+    auth 
+  let res = _topicsTopics <$> fromJSON (responseBody r :: Value)
+  pure res
