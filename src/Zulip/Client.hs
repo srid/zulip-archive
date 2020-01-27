4e2feed673ba5bddb85ddbde60d3485eb8a6b4af
Sridhar Ratnakumar <srid@srid.ca>
2020-01-26 20:46:33 -0500

    Log API requests; and refactor

--- src/Zulip/Client.hs
+++ src/Zulip/Client.hs
@@ -1,13 +1,13 @@
-{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module Zulip.Client where
 
-import Prelude hiding (Option)
 import Data.Aeson
 import Data.Aeson.TH
 import Network.HTTP.Req
 import Zulip.Internal
+import Prelude hiding (Option)
 
 baseUrl :: Text
 baseUrl = "funprog.zulipchat.com"
@@ -29,24 +29,26 @@ data Stream
   deriving (Eq, Show)
 
 data Topics
-  = Topics 
-    { _topicsResult :: Text 
-    , _topicsMsg :: Text 
-    , _topicsTopics :: [Topic] 
-    }
+  = Topics
+      { _topicsResult :: Text,
+        _topicsMsg :: Text,
+        _topicsTopics :: [Topic]
+      }
   deriving (Eq, Show)
 
-data Topic 
-  = Topic 
-    { _topicName :: Text 
-    , _topicMaxId :: Int 
-    }
+data Topic
+  = Topic
+      { _topicName :: Text,
+        _topicMaxId :: Int
+      }
   deriving (Eq, Show)
 
 $(deriveJSON fieldLabelMod ''Stream)
+
 $(deriveJSON fieldLabelMod ''Streams)
 
 $(deriveJSON fieldLabelMod ''Topics)
+
 $(deriveJSON fieldLabelMod ''Topic)
 
 type APIConfig scheme = (Url scheme, Option scheme)
@@ -60,31 +62,30 @@ demo apiKey = do
     r <- getStreams apiConfig
     case r of
       Error s -> error $ toText s
-      Success (v :: Streams) -> do 
+      Success (v :: Streams) -> do
         let streams = _streamsStreams v
-        forM streams $ \stream -> do 
-          getTopics apiConfig (_streamStreamId stream) >>= \case 
-            Error s -> error $ toText s 
-            Success topics -> do 
+        forM streams $ \stream -> do
+          getTopics apiConfig (_streamStreamId stream) >>= \case
+            Error s -> error $ toText s
+            Success topics -> do
               pure (stream, topics)
 
 getStreams :: MonadHttp m => APIConfig scheme -> m (Result Streams)
 getStreams (apiUrl, auth) = do
-  r <- req
-    GET
-    (apiUrl /: "streams")
-    NoReqBody
-    jsonResponse
-    auth
-  pure $ fromJSON (responseBody r :: Value)
+  apiGet auth $ apiUrl /: "streams"
 
-getTopics :: MonadHttp m => APIConfig scheme -> Int -> m (Result [Topic])
+getTopics :: forall m scheme. MonadHttp m => APIConfig scheme -> Int -> m (Result [Topic])
 getTopics (apiUrl, auth) streamId = do
-  r <- req 
-    GET
-    (apiUrl /: "users" /: "me" /: show streamId /: "topics")
-    NoReqBody 
-    jsonResponse 
-    auth 
-  let res = _topicsTopics <$> fromJSON (responseBody r :: Value)
-  pure res
+  fmap _topicsTopics <$> apiGet auth (apiUrl /: "users" /: "me" /: show streamId /: "topics")
+
+apiGet :: (MonadHttp m, FromJSON a) => Option scheme -> Url scheme -> m (Result a)
+apiGet auth url = do
+  liftIO $ putStrLn $ "↓ " <> show url
+  r <-
+    req
+      GET
+      url
+      NoReqBody
+      jsonResponse
+      auth
+  pure $ fromJSON (responseBody r :: Value)
