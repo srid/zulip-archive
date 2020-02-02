d370d35a02f9a2cf32e08af95d2f44a134775749
Sridhar Ratnakumar <srid@srid.ca>
2020-02-02 15:17:29 -0500

    Use a custom branch of rib

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@ let
   # To upgrade rib, go to https://github.com/srid/rib/commits/master, select the
   # revision you would like to upgrade to and set it here. Consult rib's
   # ChangeLog.md to check any notes on API migration.
-  ribRevision = "94e38226155c105b5f4d89e723af9663a4543c62";
+  ribRevision = "4e524aa";  # target-type branch
 
   inherit (import (builtins.fetchTarball "https://github.com/hercules-ci/gitignore/archive/7415c4f.tar.gz") { }) gitignoreSource;
 in {
