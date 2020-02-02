9d258c282aa433e6694fbf55fa3912daa11894b3
Sridhar Ratnakumar <srid@srid.ca>
2020-02-02 16:16:16 -0500

    Use the new Target api

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@ let
   # To upgrade rib, go to https://github.com/srid/rib/commits/master, select the
   # revision you would like to upgrade to and set it here. Consult rib's
   # ChangeLog.md to check any notes on API migration.
-  ribRevision = "4e524aa";  # target-type branch
+  ribRevision = "57163591c23dc63d4ba9aac9b947cb0fb3e41c8c";
 
   inherit (import (builtins.fetchTarball "https://github.com/hercules-ci/gitignore/archive/7415c4f.tar.gz") { }) gitignoreSource;
 in {
